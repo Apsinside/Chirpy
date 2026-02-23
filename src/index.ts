11806d7412c9ea170e49e50677c4f47df712c77c
Fabian <f.zickwolff@googlemail.com>
2026-02-23 13:51:07 +0100

    CH2: Routing | L1: Middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./middleware.js";
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("src/app"));
 app.use("/healthz", handlerReadiness);
-
+app.use(middlewareLogResponses);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import {Response, Request, NextFunction, response } from "express";
+import { stat } from "node:fs";
+
+export async function middlewareLogResponses(req: Request, res: Response, next: NextFunction){
+    res.on("finish",  ()=>{
+        const statusCode = res.statusCode;
+        if(statusCode >= 300){
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    })
+    next();
+}
